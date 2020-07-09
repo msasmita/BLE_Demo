fedffe4cc5a3c0a682a77a226dd27d702be51936
msasmita <31654027+msasmita@users.noreply.github.com>
2020-07-09 23:10:30 +0530

    adding filter for beacons

--- app/src/main/java/com/example/techolabz/ble_demo/BLEAdapter.java
+++ app/src/main/java/com/example/techolabz/ble_demo/BLEAdapter.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 
@@ -18,11 +22,11 @@ public class BLEAdapter extends ArrayAdapter<BLE> {
 
     public BLEAdapter(Context context, ArrayList<BLE> bleArrayList) {
         super(context, R.layout.row_structure, bleArrayList);
-        this.context=context;
+        this.context = context;
         this.bleArrayList = bleArrayList;
     }
 
-    public static class DataHolder{
+    public static class DataHolder {
         public TextView txtAdddress;
         public TextView txtName;
         public TextView txtuuid;
@@ -38,110 +42,135 @@ public class BLEAdapter extends ArrayAdapter<BLE> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        DataHolder holder=null;
-        if(convertView == null)
-        {
-            LayoutInflater inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView=inflater.inflate(R.layout.row_structure,null);
+        DataHolder holder = null;
+        if (convertView == null) {
 
-            holder=new DataHolder();
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.row_structure, null);
 
-            holder.txtAdddress=(TextView)convertView.findViewById(R.id.txtAddress);
-            holder.txtName=(TextView)convertView.findViewById(R.id.txtName);
-            holder.txtuuid=(TextView)convertView.findViewById(R.id.txtuuid);
-            holder.txtmajor=(TextView)convertView.findViewById(R.id.txtmajor);
-            holder.txtminor=(TextView)convertView.findViewById(R.id.txtminor);
-            holder.txtrssi=(TextView)convertView.findViewById(R.id.txtrssi);
-            holder.txtnamespaceid=(TextView)convertView.findViewById(R.id.txtnamespaceid);
-            holder.txtinstanceid=(TextView)convertView.findViewById(R.id.txtinstanceid);
-            holder.txturl=(TextView)convertView.findViewById(R.id.txturl);
-            holder.txtrawdata=(TextView)convertView.findViewById(R.id.txtrawdata);
+            holder = new DataHolder();
+
+            holder.txtAdddress = (TextView) convertView.findViewById(R.id.txtAddress);
+            holder.txtName = (TextView) convertView.findViewById(R.id.txtName);
+            holder.txtuuid = (TextView) convertView.findViewById(R.id.txtuuid);
+            holder.txtmajor = (TextView) convertView.findViewById(R.id.txtmajor);
+            holder.txtminor = (TextView) convertView.findViewById(R.id.txtminor);
+            holder.txtrssi = (TextView) convertView.findViewById(R.id.txtrssi);
+            holder.txtnamespaceid = (TextView) convertView.findViewById(R.id.txtnamespaceid);
+            holder.txtinstanceid = (TextView) convertView.findViewById(R.id.txtinstanceid);
+            holder.txturl = (TextView) convertView.findViewById(R.id.txturl);
+            holder.txtrawdata = (TextView) convertView.findViewById(R.id.txtrawdata);
 
             convertView.setTag(holder);
+        } else {
+
+            holder = (DataHolder) convertView.getTag();
         }
 
-        else{
+        BLE ble = bleArrayList.get(position);
 
-            holder=(DataHolder)convertView.getTag();
+            JSONObject postData = new JSONObject();
+            if (ble.getDeviceAddress() == "00:A0:50:5E:E2:1A" || ble.getDeviceAddress() == "00:A0:50:4D:37:4D" || ble.getDeviceAddress() == "00:A0:50:4D:36:5B") {
+
+                holder.txtAdddress.setText("Address : " + ble.getDeviceAddress());
+                holder.txtuuid.setText("Uuid : " + ble.getUuid());
+                holder.txtmajor.setText("Major : " + ble.getMajor());
+                holder.txtminor.setText("Minor : " + ble.getMinor());
+                holder.txtrssi.setText("Distannce in Meters: " + calculateDistance(ble.getRssi()) + "  " + ble.getDistanceInMeters());
+
+                holder.txtnamespaceid.setText("NamespaceId : " + ble.getNamespaceid());
+                holder.txtinstanceid.setText("InstanceId : " + ble.getInstanceid());
+                holder.txturl.setText("Url : " + ble.getUrl());
+                holder.txtrawdata.setText("Raw Data : " + ble.getRawData());
+
+                // System.out.println("HEYYY:: "+ble.getDeviceName()+"   "+ ble.getDistanceInMeters());
+
+                if (ble.getDeviceName() == null) {
+                    holder.txtName.setText("Name : " + "N/A");
+                } else {
+                    holder.txtName.setText("Name : " + ble.getDeviceName());
+                }
+
+                if (ble.getNamespaceid() == null) {
+                    holder.txtnamespaceid.setVisibility(View.GONE);
+                    holder.txtinstanceid.setVisibility(View.GONE);
+
+                } else {
+                    holder.txtnamespaceid.setVisibility(View.VISIBLE);
+                    holder.txtinstanceid.setVisibility(View.VISIBLE);
+                    holder.txtName.setText("Name : " + ble.getDeviceName() + " (Eddystone UID)");
+
+                }
+
+
+                if (ble.getUuid() == null || ble.getUuid() == "") {
+                    holder.txtuuid.setVisibility(View.GONE);
+                    holder.txtmajor.setVisibility(View.GONE);
+                    holder.txtminor.setVisibility(View.GONE);
+
+
+                } else {
+                    holder.txtuuid.setVisibility(View.VISIBLE);
+                    holder.txtmajor.setVisibility(View.VISIBLE);
+                    holder.txtminor.setVisibility(View.VISIBLE);
+                    holder.txtName.setText("Name : " + ble.getDeviceName() + " (iBeacon)");
+
+                }
+                if (ble.getUrl() == null) {
+                    holder.txturl.setVisibility(View.GONE);
+                } else {
+                    holder.txturl.setVisibility(View.VISIBLE);
+                    holder.txtName.setText("Name : " + ble.getDeviceName() + " (Eddystone Url)");
+                }
+
+
+                if (ble.getRawData() == null) {
+                    holder.txtrawdata.setVisibility(View.GONE);
+
+
+                } else {
+                    holder.txtrawdata.setVisibility(View.VISIBLE);
+                    holder.txtName.setText("Name : " + ble.getDeviceName() + " (Raw Data)");
+                }
+
+
+               try{
+                   if(ble.getDeviceAddress() == "00:A0:50:5E:E2:1A") {
+                       postData.put("1A", calculateDistance(ble.getRssi()));
+                   }else if(ble.getDeviceAddress() == "00:A0:50:4D:37:4D"){
+                       postData.put("4D", calculateDistance(ble.getRssi()));
+                   }else if(ble.getDeviceAddress() == "00:A0:50:4D:36:5B"){
+                       postData.put("5B", calculateDistance(ble.getRssi()));
+                   }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                return convertView;
+            }else {
+                return convertView;
+            }
         }
 
-       BLE ble= bleArrayList.get(position);
+        //return convertView;
 
 
-        holder.txtAdddress.setText("Address : "+ble.getDeviceAddress());
-        holder.txtuuid.setText("Uuid : "+ble.getUuid());
-        holder.txtmajor.setText("Major : "+ble.getMajor());
-        holder.txtminor.setText("Minor : "+ble.getMinor());
-        holder.txtrssi.setText("Rssi : "+ble.getRssi());
-        holder.txtnamespaceid.setText("NamespaceId : "+ble.getNamespaceid());
-        holder.txtinstanceid.setText("InstanceId : "+ble.getInstanceid());
-        holder.txturl.setText("Url : "+ble.getUrl());
-        holder.txtrawdata.setText("Raw Data : "+ble.getRawData());
+    public Double calculateDistance(String rssi) {
 
-
-        if(ble.getDeviceName()==null){
-            holder.txtName.setText("Name : "+"N/A");
-        }
-        else{
-            holder.txtName.setText("Name : "+ble.getDeviceName());
+        int txPower = -59 ;//hard coded power value. Usually ranges between -59 to -65
+        Double inum = Double.parseDouble(rssi);
+        if (inum == 0) {
+            return -1.0;
         }
 
-        if(ble.getNamespaceid()==null){
-            holder.txtnamespaceid.setVisibility(View.GONE);
-            holder.txtinstanceid.setVisibility(View.GONE);
-
+        Double ratio = (inum * 1.0) / txPower;
+        if (ratio < 1.0) {
+            return Math.pow(ratio,10);
         }
-
         else {
-            holder.txtnamespaceid.setVisibility(View.VISIBLE);
-            holder.txtinstanceid.setVisibility(View.VISIBLE);
-            holder.txtName.setText("Name : "+ble.getDeviceName()+" (Eddystone UID)");
-
+            Double distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
+            return distance;
         }
-
-
-        if(ble.getUuid()==null || ble.getUuid()==""){
-            holder.txtuuid.setVisibility(View.GONE);
-            holder.txtmajor.setVisibility(View.GONE);
-            holder.txtminor.setVisibility(View.GONE);
-
-
-        }
-
-        else {
-            holder.txtuuid.setVisibility(View.VISIBLE);
-            holder.txtmajor.setVisibility(View.VISIBLE);
-            holder.txtminor.setVisibility(View.VISIBLE);
-
-            holder.txtName.setText("Name : "+ble.getDeviceName()+" (iBeacon)");
-
-        }
-        if(ble.getUrl()==null){
-            holder.txturl.setVisibility(View.GONE);
-
-
-        }
-        else{
-            holder.txturl.setVisibility(View.VISIBLE);
-            holder.txtName.setText("Name : "+ble.getDeviceName()+" (Eddystone Url)");
-
-
-        }
-
-
-        if(ble.getRawData()==null){
-            holder.txtrawdata.setVisibility(View.GONE);
-
-
-        }
-        else{
-            holder.txtrawdata.setVisibility(View.VISIBLE);
-            holder.txtName.setText("Name : "+ble.getDeviceName()+" (Raw Data)");
-
-
-        }
-
-
-        return convertView;
     }
 }
